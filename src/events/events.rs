684af42473286af1f2b5efd07998d052f38d78bc
web3nish <const.nishant@gmail.com>
2025-04-09 18:18:37 +0400

    combining user operation event

--- src/events/events.rs
+++ src/events/events.rs
@@ -1,5 +1,8 @@
 use alloy::sol;
 use serde::Serialize;
+
+use alloy::primitives::{aliases::U48, Address, B256, U256};
+
 // ✅ Define Solidity Events
 sol! {
     // Events for User Sponsored and Gas Refunded from Paymaster
@@ -17,4 +20,31 @@ sol! {
 
     #[derive(Debug, Serialize)] 
     event UserOperationEvent(bytes32 indexed userOpHash, address indexed sender, address indexed paymaster, uint256 nonce, bool success, uint256 actualGasCost, uint256 actualGasUsed);
+}
+
+// custom events
+#[derive(Debug, Clone, Serialize)]
+pub struct CombinedUserOpEvent {
+    // From UserOperationEvent
+    pub user_op_hash: B256,
+    pub sender: Address,
+    pub paymaster: Address,
+    pub paymaster_type: String,
+    pub nonce: U256,
+    pub success: bool,
+    pub actual_gas_cost: U256,
+    pub actual_gas_used: U256,
+
+    // The user's account used to pay for the gas
+    pub deducted_user: Option<Address>,
+
+    // From GasBalanceDeducted (optional)
+    pub deducted_amount: Option<U256>,
+    pub deducted_premium: Option<U256>,
+
+    // From PaidGasInTokens (optional)
+    pub token: Option<Address>,
+    pub token_charge: Option<U256>,
+    pub applied_markup: Option<U48>,
+    pub exchange_rate: Option<U256>,
 }
--- src/processor/handler.rs
+++ src/processor/handler.rs
@@ -1,10 +1,10 @@
 use alloy_sol_types::SolEvent;
 use alloy::{
-    primitives::Log as AlloyLog,
+    primitives::{Address, Log as AlloyLog, B256, U256},
     rpc::types::Log as RpcLog,
 };
 use scs_aa_indexer::events::events::{
-    GasBalanceDeducted, RefundProcessed, UserOperationEvent, UserOperationSponsored, PaidGasInTokens
+    GasBalanceDeducted, RefundProcessed, UserOperationEvent, UserOperationSponsored, PaidGasInTokens, CombinedUserOpEvent
 };
 use crate::storage::Storage;
 
@@ -20,50 +20,57 @@ pub async fn process_event<S: Storage> (event_name: &str, log: &RpcLog, previous
         "UserOperationEvent" => {
             if let Some(prev_log) = previous_log.take() {
                 let prev_log = AlloyLog::from(prev_log.clone());
+
+                let mut combine_log = CombinedUserOpEvent {
+                    user_op_hash: B256::ZERO,
+                    sender: Address::ZERO,
+                    paymaster: Address::ZERO,
+                    paymaster_type: String::new(),
+                    nonce: U256::ZERO,
+                    success: false,
+                    actual_gas_cost: U256::ZERO,
+                    actual_gas_used: U256::ZERO,
+                    deducted_user: None,
+                    deducted_amount: None,
+                    deducted_premium: None,
+                    token: None,
+                    token_charge: None,
+                    applied_markup: None,
+                    exchange_rate: None,
+                };
+
+                // Check if the previous log was a GasBalanceDeducted event from sponsership paymaster
                 if let Ok(event) = GasBalanceDeducted::decode_log(&prev_log, true) {
-                    println!(
-                        "✅ Decoded GasBalanceDeducted:\n\
-                        - user: {:?}\n\
-                        - amount: {}\n\
-                        - premium: {}\n\
-                        - mode: {}",
-                        event.user, event.amount, event.premium, event.mode
-                    );
-                    storage.store(&event, "GasBalanceDeducted").await;
+                    combine_log.deducted_user = Some(event.user);
+                    combine_log.deducted_amount = Some(event.amount);
+                    combine_log.deducted_premium = Some(event.premium);
+                    combine_log.paymaster_type = "SPONSORSHIP".to_string();
                 }
+
+                // Check if the previous log was a PaidGasInTokens event from token paymaster
                 if let Ok(event) = PaidGasInTokens::decode_log(&prev_log, true) {
-                    println!(
-                        "✅ Decoded PaidGasInTokens:\n\
-                        - user: {:?}\n\
-                        - token: {}\n\
-                        - tokenCharge : {}\n\
-                        - appliedMarkup: {}\n\
-                        - exchangeRate: {}",
-                        event.user, event.token, event.tokenCharge, event.appliedMarkup, event.exchangeRate
-                    );
-                    storage.store(&event, "PaidGasInTokens").await;
+                    combine_log.deducted_user = Some(event.user);
+                    combine_log.token = Some(event.token);
+                    combine_log.token_charge = Some(event.tokenCharge);
+                    combine_log.applied_markup = Some(event.appliedMarkup);
+                    combine_log.exchange_rate = Some(event.exchangeRate);
+                    combine_log.paymaster_type = "Token".to_string();
                 }
+
                 let user_op_log = AlloyLog::from(log.clone());
                 println!("✅ Matched UserOperationEvent for previous event");
                 if let Ok(event) = UserOperationEvent::decode_log(&user_op_log, false) {
-                    println!(
-                        "✅ Decoded UserOperationEvent:\n\
-                        - userOpHash: {:?}\n\
-                        - sender: {:?}\n\
-                        - paymaster: {:?}\n\
-                        - nonce: {}\n\
-                        - success: {}\n\
-                        - actualGasCost: {}\n\
-                        - actualGasUsed: {}",
-                        event.userOpHash,
-                        event.sender,
-                        event.paymaster,
-                        event.nonce,
-                        event.success,
-                        event.actualGasCost,
-                        event.actualGasUsed
-                    );
-                    storage.store(&event, "UserOperationEvent").await;
+
+                    combine_log.user_op_hash = event.userOpHash;
+                    combine_log.sender = event.sender;
+                    combine_log.paymaster = event.paymaster;
+                    combine_log.nonce = event.nonce;
+                    combine_log.success = event.success;
+                    combine_log.actual_gas_cost = event.actualGasCost;
+                    combine_log.actual_gas_used = event.actualGasUsed;
+                    
+                    println!("✅ Combined Event: {:?}", &combine_log);
+                    storage.store(&combine_log, "CombinedUserOperation").await;
                     *previous_log = None;
                 }
             } else {
