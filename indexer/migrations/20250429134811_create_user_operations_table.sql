3aaf778036e1f3b8d9779987bd37d437bd972a2c
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2025-04-29 18:22:02 +0100

    fix hypertable

--- indexer/migrations/20250429134811_create_user_operations_table.sql
+++ indexer/migrations/20250429134811_create_user_operations_table.sql
@@ -1,22 +1,25 @@
 
 CREATE TABLE IF NOT EXISTS pm_user_operations (
-    user_op_hash CHAR(66) NOT NULL PRIMARY KEY,
     time TIMESTAMPTZ NOT NULL,
+    user_op_hash CHAR(66) NOT NULL,
     user_operation JSONB NOT NULL,
     project_id VARCHAR(30) NOT NULL,
     paymaster_mode VARCHAR(20) NOT NULL,
-    paymaster_id VARCHAR(30) NOT NULL,
-    token_address CHAR(42) TEXT,
-    fund_type VARCHAR(20) NOT NULL,
+    paymaster_id VARCHAR(30),
+    token_address CHAR(42),
+    fund_type VARCHAR(20),
     status VARCHAR(10) NOT NULL,
-    data_source VARCHAR(20) TEXT NOT NULL,
+    data_source VARCHAR(20) NOT NULL,
     metadata JSONB NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Create hypertable for timeseries data
-SELECT create_hypertable('pm_user_operations', 'time');
+SELECT create_hypertable('pm_user_operations', by_range('time'));
+
+CREATE UNIQUE INDEX idx_user_op_hash
+  ON pm_user_operations(user_op_hash, time);
 
 CREATE OR REPLACE FUNCTION trigger_set_updated_at()
 RETURNS TRIGGER AS $$
